c3e7ecc7d852b9931c54ada3356a86c8209f481d
Jayakrishnan Mekkattillam <jmekkatt@redhat.com>
2022-05-05 19:44:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-From flask import Flask
+from flask import Flask
+import time
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
+    time.sleep(5)
     return 'Hey, we have Flask in a Docker container -- By Jaya!'
 
-
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
